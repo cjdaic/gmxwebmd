d6613bb42e5abdf3e460dd21c741e89b2cfbf240
cjdai <cjdaic@outlook.com>
2023-04-20 00:26:10 +0800

    update in 4/19

--- save.php
+++ save.php
@@ -2,16 +2,16 @@
 // 从POST请求中获取json数据
 $json_data = file_get_contents("php://input");
 // 检查json数据是否有效
-if (strlen($json_data) > 0 && isValidJSON($json_data)) {
+if (strlen($json_data) > 0 && json_decode($json_data) != null) {
   // 将json数据转换为数组
   $data = json_decode($json_data, true);
   // 获取id和value
   $id = $data['id'];
-  $value = $data['value'];
+  $value = $data['valu'];
   // 打开或创建mydata.json文件
-  $file = fopen("mydata.json", "w");
+  $file = fopen("usr/usr.json", "w");
   // 检查文件是否可写
-  if (is_writable($file)) {
+  if (is_writable("usr/usr.json")) {
     // 将id和value写入文件
     fwrite($file, "$id: $value\n");
     // 关闭文件
@@ -23,12 +23,7 @@ if (strlen($json_data) > 0 && isValidJSON($json_data)) {
     echo "Error: file is not writable";
   }
 } else {
-  // 返回错误信息
-  echo "Error: invalid json data";
+  echo "nmsl";
 }
-// 定义一个函数，用于检查json数据是否有效
-function isValidJSON($str) {
-   json_decode($str);
-   return json_last_error() == JSON_ERROR_NONE;
-}
+
 ?>
